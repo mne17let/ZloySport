12395b1392012120ee097854d40f1355c12d7adf
mne_17_let <vladimirsemovskih@gmail.com>
2022-08-28 02:41:13 +0400

    Сделал компоузабл топбара

--- app/src/main/java/com/zloysport/ui/Composables.kt
+++ app/src/main/java/com/zloysport/ui/Composables.kt
@@ -1,14 +1,18 @@
 package com.zloysport.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.zloysport.R
 
 @Composable
@@ -29,10 +33,25 @@ fun TitleBar(
     rightIcon: Int,
 ) {
     Row(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp, horizontal = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(painter = painterResource(id = leftIcon), contentDescription = "")
-        Text(text = stringResource(id = title))
-        Icon(painter = painterResource(id = rightIcon), contentDescription = "")
+        Icon(
+            modifier = Modifier.width(36.dp),
+            painter = painterResource(id = leftIcon),
+            contentDescription = ""
+        )
+        Text(
+            modifier = Modifier.background(Color.Green),
+            text = stringResource(id = title),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Icon(
+            modifier = Modifier.width(36.dp),
+            painter = painterResource(id = rightIcon),
+            contentDescription = ""
+        )
     }
 }
