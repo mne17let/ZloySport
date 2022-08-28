99bf38e367b45fbd0d1c2976f65354d414e27adc
mne17let <vladimirsemovskih@gmail.com>
2022-08-29 01:48:30 +0400

    Правки и форма кнопки

--- app/src/main/java/com/zloysport/ui/Composables.kt
+++ app/src/main/java/com/zloysport/ui/Composables.kt
@@ -1,22 +1,21 @@
 package com.zloysport.ui
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zloysport.R
 
-@Preview
 @Composable
 fun EnterTrainingNameScreen() {
     Column(
@@ -25,27 +24,57 @@ fun EnterTrainingNameScreen() {
     ) {
         TitleBar(
             R.drawable.ic_back,
-            R.string.trainings_title,
+            R.string.create_training_title,
             R.drawable.ic_close,
         )
 
-        EnterTrainingName()
+        EnterTrainingNameTextField()
         ConfirmButton(R.string.confirm_button_title)
     }
 }
 
-@Preview
 @Composable
-fun EnterTrainingName() {
+fun TitleBar(
+    leftIcon: Int,
+    title: Int,
+    rightIcon: Int,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp, horizontal = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            modifier = Modifier
+                .size(24.dp),
+            painter = painterResource(id = leftIcon),
+            contentDescription = ""
+        )
+        Text(
+            text = stringResource(id = title),
+            fontSize = 22.sp,
+        )
+        Icon(
+            modifier = Modifier
+                .size(24.dp),
+            painter = painterResource(id = rightIcon),
+            contentDescription = ""
+        )
+    }
+}
+
+@Composable
+fun EnterTrainingNameTextField() {
 
     var valueText by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 32.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-
-    ) {
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
         Text(
             modifier = Modifier
                 .fillMaxWidth()
@@ -65,57 +94,22 @@ fun EnterTrainingName() {
             }
         )
     }
-
-
 }
 
 @Composable
-fun TitleBar(
-    leftIcon: Int,
-    title: Int,
-    rightIcon: Int,
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 16.dp, horizontal = 16.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            modifier = Modifier
-                .width(24.dp)
-                .height(24.dp),
-            painter = painterResource(id = leftIcon),
-            contentDescription = ""
-        )
-        Text(
-            text = stringResource(id = title),
-            fontSize = 22.sp,
-        )
-        Icon(
-            modifier = Modifier
-                .width(24.dp)
-                .height(24.dp),
-            painter = painterResource(id = rightIcon),
-            contentDescription = ""
-        )
-    }
-
-
-}
-
-@Composable
-fun ConfirmButton(@StringRes stringResourceId: Int) {
-
+fun ConfirmButton(@StringRes textResourceId: Int) {
     Button(
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 32.dp)
-            .padding(bottom = 106.dp)
-            .height(60.dp),
+            .padding(bottom = 32.dp)
+            .clip(shape = RoundedCornerShape(corner = CornerSize(16.dp))),
         onClick = { }) {
-        Text(text = stringResource(stringResourceId))
+        Text(
+            modifier = Modifier
+                .padding(vertical = 16.dp),
+            text = stringResource(textResourceId)
+        )
     }
 
 }
