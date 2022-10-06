492be4a86052322b0c1b0fe09020670336042332
mne17let <vladimirsemovskih@gmail.com>
2022-10-07 03:00:30 +0400

    Начало вёрстки круглого слайдера

--- app/src/main/java/com/zloysport/ui/setupreps/composables/ScreenDrill.kt
+++ app/src/main/java/com/zloysport/ui/setupreps/composables/ScreenDrill.kt
@@ -1,30 +1,36 @@
 package com.zloysport.ui.setupreps.composables
 
+import android.graphics.Paint
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.zloysport.ui.theme.DarkGreen
-import com.zloysport.ui.theme.DarkestBlue
-import com.zloysport.ui.theme.LightBlue
-import com.zloysport.ui.theme.LightGreen
+import com.zloysport.ui.theme.*
+import java.lang.Math.*
+import kotlin.math.atan2
 
 @Composable
 fun ScreenDrill() {
@@ -49,15 +55,21 @@ fun ScreenDrill() {
                 )
             )
         )
-        RangeInfo()
+        RangeInfo(
+            Range(
+                49,
+                false
+            )
+        )
     }
 }
 
 @Composable
 private fun DrillInfo(drillInfo: DrillInfo) {
-    LazyRow(modifier = Modifier
-        .padding(vertical = 20.dp)
-        ) {
+    LazyRow(
+        modifier = Modifier
+            .padding(vertical = 20.dp)
+    ) {
         items(drillInfo.rangeList.size) {
             val range = drillInfo.rangeList[it]
             val modifier = Modifier
@@ -71,9 +83,9 @@ private fun DrillInfo(drillInfo: DrillInfo) {
                     .clip(shape = CircleShape)
                     .background(backColor)
                     .clickable(enabled = true,
-                    interactionSource = MutableInteractionSource(),
-                    indication = rememberRipple(),
-                    onClick = {}),
+                        interactionSource = MutableInteractionSource(),
+                        indication = rememberRipple(),
+                        onClick = {}),
                 contentAlignment = Alignment.Center
             ) {
                 Text(
@@ -87,9 +99,156 @@ private fun DrillInfo(drillInfo: DrillInfo) {
 }
 
 @Composable
-private fun RangeInfo() {
+fun RangeInfo(range: Range) {
 
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Canvas(
+            modifier = Modifier
+                .padding(24.dp)
+                .fillMaxSize()
+//                .background(DarkestBlue)
+        ) {
+            val strokeWidth = 20f
+            val topLeftX = (size.width / 2f) - (size.minDimension / 2f) + (strokeWidth / 2)
+            val topLeftY = (size.height / 2f) - (size.minDimension / 2f) + (strokeWidth / 2)
+            val topLeft = Offset(topLeftX, topLeftY)
+            drawArc(
+                color = DarkGray,
+                startAngle = 0f,
+                sweepAngle = -180f,
+                useCenter = false,
+                size = Size(size.minDimension - strokeWidth, size.minDimension - strokeWidth),
+                alpha = 1f,
+                style = Stroke(width = strokeWidth, cap = StrokeCap.Round),
+                topLeft = topLeft
+            )
+
+            val center = Offset(size.width / 2, size.height / 2)
+
+            val radius = size.minDimension / 2 - strokeWidth / 2
+            val longRadius = size.minDimension / 2 + 16 - strokeWidth / 2
+            val shortRadius = size.minDimension / 2 - 16 - strokeWidth / 2
+//            val oneAngle = (180 / range.count) * Math.PI / 180
+            val oneAngle = PI / range.count
+            val startX = 0f
+            val startY = size.height / 2
+
+            val startStart = Offset(startX + 100, startY + 0)
+            val startEnd = Offset(startX - 100, startY + 0)
+
+            var angle = 0.0
+
+            for (action in 0..range.count) {
+                val nextX = size.width / 2 - radius * Math.cos(angle)
+                val nextY = size.height / 2 - radius * Math.sin(angle)
+
+                val nextXStart = size.width / 2 - longRadius * Math.cos(angle)
+                val nextYStart = size.height / 2 - longRadius * Math.sin(angle)
+
+                val nextXEnd = size.width / 2 - shortRadius * Math.cos(angle)
+                val nextYEnd = size.height / 2 - shortRadius * Math.sin(angle)
+
+                if (range.count < 20) {
+                    drawLine(
+                        Color.Black,
+                        start = Offset(nextXStart.toFloat(), nextYStart.toFloat()),
+                        end = Offset(nextXEnd.toFloat(), nextYEnd.toFloat()),
+                        strokeWidth = 8f
+                    )
+
+                    drawCircle(
+                        color = Color.Cyan,
+                        center = Offset(nextX.toFloat(), nextY.toFloat()),
+                        radius = 4f
+                    )
+
+                    drawIntoCanvas {
+                        it.nativeCanvas.drawText(
+                            "$action",
+                            nextX.toFloat(),
+                            nextY.toFloat(),
+                            Paint().apply {
+                                textSize = 24f
+                            }
+                        )
+                    }
+
+                } else if (range.count < 50) {
+                    if (range.count == action || action % 5 == 0) {
+                        drawLine(
+                            Color.Black,
+                            start = Offset(nextXStart.toFloat(), nextYStart.toFloat()),
+                            end = Offset(nextXEnd.toFloat(), nextYEnd.toFloat()),
+                            strokeWidth = 8f
+                        )
+
+                        drawCircle(
+                            color = Color.Cyan,
+                            center = Offset(nextX.toFloat(), nextY.toFloat()),
+                            radius = 4f
+                        )
+
+                        drawIntoCanvas {
+                            it.nativeCanvas.drawText(
+                                "$action",
+                                nextX.toFloat(),
+                                nextY.toFloat(),
+                                Paint().apply {
+                                    textSize = 24f
+                                }
+                            )
+                        }
+                    }
+                } else {
+                    if (range.count == action || action % 10 == 0) {
+                        drawLine(
+                            Color.Black,
+                            start = Offset(nextXStart.toFloat(), nextYStart.toFloat()),
+                            end = Offset(nextXEnd.toFloat(), nextYEnd.toFloat()),
+                            strokeWidth = 8f
+                        )
+
+                        drawCircle(
+                            color = Color.Cyan,
+                            center = Offset(nextX.toFloat(), nextY.toFloat()),
+                            radius = 4f
+                        )
+
+                        drawIntoCanvas {
+                            it.nativeCanvas.drawText(
+                                "$action",
+                                nextX.toFloat(),
+                                nextY.toFloat(),
+                                Paint().apply {
+                                    textSize = 24f
+                                }
+                            )
+                        }
+                    }
+                }
+
+                angle += oneAngle
+            }
+
+            drawIntoCanvas {
+                it.nativeCanvas.drawText(
+//                    "${angle * 180 / Math.PI}",
+//                    "${oneAngle * 180 / PI} || ${oneAngle * 180 / PI * 180}",
+                    "|| ${oneAngle * 180 / PI * range.count}",
+                    size.width / 2,
+                    size.height / 2,
+                    Paint().apply {
+                        textSize = 24f
+                    }
+                )
+            }
+        }
+    }
 }
+
 
 data class DrillInfo(
     val rangeList: List<Range>
--- app/src/main/java/com/zloysport/ui/theme/Colors.kt
+++ app/src/main/java/com/zloysport/ui/theme/Colors.kt
@@ -6,5 +6,8 @@ val LightBlue = Color(0xFFE8F4FB)
 val Blue = Color(0xFF1092DC)
 val DarkBlue = Color(0xFF0067A2)
 val DarkestBlue = Color(0xFF004973)
+
 val LightGreen = Color(0xFFB2EC9E)
 val DarkGreen = Color(0xFF368B00)
+
+val DarkGray = Color(0xFF8C8F8B)
